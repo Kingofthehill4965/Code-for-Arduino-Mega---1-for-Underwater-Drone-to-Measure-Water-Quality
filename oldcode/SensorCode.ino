593baee921719c8a62211680484e38d37d4e2bbf
Kingofthehill4965 <167987818+Kingofthehill4965@users.noreply.github.com>
2024-06-20 12:41:14 +0530

    Added sonar for depth

--- oldcode/SensorCode.ino
+++ oldcode/SensorCode.ino
@@ -6,10 +6,17 @@
 #include <DallasTemperature.h>
 #include <SD.h>
 #include <SPI.h>
- 
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
 #define ONE_WIRE_BUS 2
 #define TdsSensorPin A1
 GravityTDS gravityTds;
+
+float significantchangeid;
+ 
+unsigned char data[4]={};
+char c;
 
 float tempC; 
 float temperature = tempC,tdsValue = 0;
@@ -31,25 +38,73 @@ float prevTds = 0;
 float prevPh = 0;
 float prevTurbidity = 0;
 
+float distance;
+float tempchange = 0;
+float tdschange = 0;
+float phchange = 0;
+float turbiditychange = 0;
+
+const int motorPin1 = 40;
+const int motorpin2 = 41;
+const int motorpin3 = 42;
+const int motorpin4 = 43;
+const int motorpin5 = 44;
+const int motorpin6 = 45;
+const int motorpin7 = 46;
+const int motorpin8 = 47;
+const int motorpin9 = 48;
+const int motorpin10 = 49;
+const int rotormotorpin = 50;
+
 int chipSelect= 53;
 File mySensorData;
+
+SoftwareSerial mySerial2(11,10); // RX, TX 
+SoftwareSerial mySerial(3, 2);
+Adafruit_GPS GPS(&mySerial);
 
 OneWire oneWire(ONE_WIRE_BUS);
 
 DallasTemperature sensors(&oneWire);
 
 void setup() {
-    Serial.begin(9600);
-    gravityTds.setPin(TdsSensorPin);
-    gravityTds.setAref(5.0);
-    gravityTds.setAdcRange(1024);
-    gravityTds.begin(); 
-    pinMode(10,OUTPUT);
-    SD.begin(chipSelect);
-    sensors.begin();
+  gravityTds.setPin(TdsSensorPin);
+  gravityTds.setAref(5.0);
+  gravityTds.setAdcRange(1024);
+  gravityTds.begin(); 
+  pinMode(10,OUTPUT);
+  SD.begin(chipSelect);
+  sensors.begin();
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+  pinMode(motorPin5, OUTPUT);
+  pinMode(motorPin6, OUTPUT);
+  pinMode(motorPin7, OUTPUT);
+  pinMode(motorPin8, OUTPUT);
+  pinMode(motorPin9, OUTPUT);
+  pinMode(motorPin10, OUTPUT);
+  pinMode(rotormotorpin, OUTPUT);
+  significantchangeid = 1;
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
 }
 
 void loop() {  
+  for(int i=0;i<4;i++){
+       data[i]=mySerial2.read();
+     }
+  while(mySerial2.read()==0xff);
+    mySerial2.flush();
+  if(data[0]==0xff){
+    int sum;
+    sum=(data[0]+data[1]+data[2])&0x00FF;
+    if(sum==data[3]){
+        distance=(data[1]<<8)+data[2];
+        distance=distance / 10;
+        }
+     }
   mySensorData = SD.open("test.txt",FILE_WRITE);
   gravityTds.setTemperature(temperature);
   gravityTds.update();
@@ -77,37 +132,129 @@ void loop() {
   float tempF = sensors.getTempFByIndex(0);
   int sensorValue = analogRead(A2);
   int turbidity =map(sensorValue,0,700,100,0);  
-if(mySensorData){
-  Serial.println("1");
-  mySensorData.print(tdsValue,0);
-  mySensorData.println(",");
-  mySensorData.print(ph_act);
-  mySensorData.println(",");
-  mySensorData.print(tempC);
-  mySensorData.println(",");
-  mySensorData.print(tempF);
-  mySensorData.println(",");
-  mySensorData.print(turbidity);
-  mySensorData.println(",");
-  mySensorData.print(Ec);
+  if(mySensorData){
+    MySensorData.println("1");
+    mySensorData.print(tdsValue,0);
+    mySensorData.println(",");
+    mySensorData.print(ph_act);
+    mySensorData.println(",");
+    mySensorData.print(tempC);
+    mySensorData.println(",");
+    mySensorData.print(tempF);
+    mySensorData.println(",");
+    mySensorData.print(turbidity);
+    mySensorData.println(",");
+    mySensorData.print(Ec);
+    mySensorData.print("Distance from riverbed : ");
+    mySensorData.print(distance);
+    mySensorData.print("cm");
+    clearGPS();
+    while (!GPS.newNMEAreceived()) {
+      c = GPS.read();
+    }
+    GPS.parse(GPS.lastNMEA());
+    MySensorData.print("Time: ");
+    MySensorData.print(GPS.hour, DEC);
+    MySensorData.print(':');
+    MySensorData.print(GPS.minute, DEC);
+    MySensorData.print(':');
+    MySensorData.print(GPS.seconds, DEC);
+    MySensorData.print('.');
+    MySensorDataorData.println(GPS.milliseconds);
+    MySensorData.print("Date: ");
+    MySensorData.print(GPS.day, DEC);
+    MySensorData.print('/');
+    MySensorData.print(GPS.month, DEC);
+    MySensorData.print("/20");
+    MySensorData.println(GPS.year, DEC);
+    MySensorData.print("Fix: ");
+    MySensorData.print(GPS.fix);
+    MySensorData.print(" quality: ");
+    MySensorData.println(GPS.fixquality);
+    MySensorData.print("Satellites: ");
+    MySensorData.println(GPS.satellites);
+    if (GPS.fix) {
+      MySensorData.print("Location: ");
+      MySensorData.print(GPS.latitude, 4);
+      MySensorData.print(GPS.lat);
+      MySensorData.print(", ");
+      MySensorData.print(GPS.longitude, 4);
+      MySensorData.println(GPS.lon);
+      MySensorData.print("Google Maps location: ");
+      MySensorData.print(GPS.latitudeDegrees, 4);
+      MySensorData.print(", ");
+      MySensorData.println(GPS.longitudeDegrees, 4);
+      MySensorData.print("Speed (knots): ");
+      MySensorData.println(GPS.speed);
+      MySensorData.print("Heading: ");
+      MySensorData.println(GPS.angle);
+      MySensorData.print("Altitude: ");
+      MySensorData.println(GPS.altitude);
+    }
+    MySensorData.println("-------------------------------------");
+
+  tempchange = tempC - prevTemp;
+  tdschange = tdsValue - prevTds;
+  phchange = ph_act - prevPh;
+  turbiditychange = turbidity - prevTurbidity;
+  if (abs(tempchange) > temp_threshold){
+    log = "Temperature," + " Change : " + tempchange + ", Previous : " + prevTemp + ", Current : " + tempC;
+    significantchange = true;
+    prevTemp = tempC;
+  }
+  if (abs(tdschange) > tds_threshold){
+    log = "TDS," + " Change : " + tdschange + ", Previous : " + prevTds + ", Current : " + tdsValue;
+    significantchange = true;
+    prevTds = tdsValue;
+  }
+  if (abs(phchange) > ph_threshold){
+    log = "PH," + " Change : " + phchange + ", Previous : " + prevPh + ", Current : " + ph_act;
+    significantchange = true;
+    prevPh = ph_act;
+  }
+  if (abs(turbiditychange) > turbidity_threshold){
+    log = "Turbidity," + " Change : " + turbiditychange + ", Previous : " + prevTurbidity + ", Current : " + turbidity;
+    significantchange = true;
+    prevTurbidity = turbidity;
+  }
+  if (significantchange){
+    for (int i=1; i<11; i++){
+      if (significantchangeid == i){
+        significantchange = false;
+        digitalWrite(motorPin1 + i, HIGH);
+        delay(15000);
+        mySensorData.println("Significant change, ");
+        mySensorData.println("id : -");
+        mySensorData.print(significantchangeid);
+        mySensorData.print("reason : ");
+        mySensorData.print(log);
+        digitalWrite(motorPin1 + i, LOW);
+        significantchangeid = significantchangeid + 1;
+        digitalWrite(rotormotorpin, HIGH);
+        delay(2500);
+        digitalWrite(rotormotorpin, LOW);-
+        break;
+      }
+    }
+    if (significantchangeid == 11){
+      significantchange = false;
+    }
+    else {
+      mySensorData.print("ERROR 404 : Autonomous Sampling error.")
+    }
+  }
 }
 delay(10);
 }
 
-void samplecollection(){
-  if (abs(tempC - prevTemp) > temp_threshold){
-    significantchange = true;
-    prevTemp = tempC;
+void clearGPS() {
+  while (!GPS.newNMEAreceived()) {
+    c = GPS.read();
   }
-  if (abs(tdsValue - prevTds) > tds_threshold){
-    significantchange = true;
-    prevTds = tdsValue;
+  GPS.parse(GPS.lastNMEA());
+
+  while (!GPS.newNMEAreceived()) {
+    c = GPS.read();
   }
-  if (abs(ph_act - prevPh) > ph_threshold){
-    significantchange = true;
-    prevPh = ph_act;
-  if (abs(turbidity - prevTurbidity) > turbidity_threshold){
-    significantchange = true;
-    prevTurbidity = turbidity;
-  }
+  GPS.parse(GPS.lastNMEA());
 }
