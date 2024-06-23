6158f0aaba4f4cce3efb1c78e984792c95525d5b
Kingofthehill4965 <167987818+Kingofthehill4965@users.noreply.github.com>
2024-06-23 19:31:36 +0530

    Make the code more readable
    Last small change i promise

--- separate sensor code/pH.ino
+++ separate sensor code/pH.ino
@@ -1,16 +1,20 @@
 #define pHSensorPin A0;
+
 float ph_calibration_value = 21.34;
-unsigned long int avgval; 
+
+unsigned long int avgval;
+
 int buffer_arr[10],temp;
-void setup() 
-{
- Serial.begin(9600);  
+
+void setup(){
+ Serial.begin(9600);
 }
 void loop(){
   for(int b=0;b<10;b++){
     buffer_arr[b]=analogRead(pHSensorPin);
     delay(30);
   }
+ 
   for(int c=0;c<9;c++){
     for(int d=c+1;d<10;d++){
       if(buffer_arr[c]>buffer_arr[d]){
@@ -20,13 +24,18 @@ void loop(){
       }
     }
   }
+  
   avgval=0;
+ 
   for(int e=2;e<8;e++){
     avgval+=buffer_arr[e];
   }
+  
   float volt=(float)avgval * 5.0/1024/6;
   float ph = -5.70 * volt + ph_calibration_value;
+  
   Serial.print("pH: ");
   Serial.println(ph);
+  
   delay(1000);
 }
