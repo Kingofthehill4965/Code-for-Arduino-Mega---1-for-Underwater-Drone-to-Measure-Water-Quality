316385d8dc75e63deae317e97ffcac9b6f7a3255
Kingofthehill4965 <167987818+Kingofthehill4965@users.noreply.github.com>
2024-06-24 13:30:24 +0530

    Rename stuff

--- separate sensor code/pressure.ino
+++ separate sensor code/pressure.ino
@@ -1,12 +1,12 @@
-int pressurepin = A8;
+#define pressureSensorPin = A8;
 
 void setup(){
   Serial.begin(9600);
 }
 
 void loop(){
-  int pressuresensorValue = analogRead(pressurepin);
-  float voltage = pressuresensorValue * (5.0 / 1023.0);
+  int pressuresensorValue = analogRead(pressureSensorPin);
+  float voltage = pressureSensorValue * (5.0 / 1023.0);
   float baselinevoltage = 0;
   float pressure = (voltage - baselinevoltage) * (100/ 4.5 - baselinevoltage);
   mySensorData.print("Pressure : ");
