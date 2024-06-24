d6544bea3a1616c023c4da7ce1769c1842bdd569
Kingofthehill4965 <167987818+Kingofthehill4965@users.noreply.github.com>
2024-06-24 12:58:16 +0530

    Written pressure code.
    Yes, for testing. Will rename, debug and verify stuff later.

---
+++ separate sensor code/pressure.ino
@@ -0,0 +1,15 @@
+int pressurepin = A8;
+
+void setup(){
+  pinMode(pressurepin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  int pressuresensorValue = analogRead(pressurepin);
+  float voltage = pressuresensorValue * (5.0 / 1023.0);
+  float baselinevoltage = 0;
+  float pressure = (voltage - baselinevoltage) * (100/ 4.5 - baselinevoltage);
+  mySensorData.print("Pressure : ");
+  mySensorData.print(pressure);
+}
